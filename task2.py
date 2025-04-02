068c2ee35fd61fb188ead19929cb1eab631f0dc0
Shamil <vergasov.shamil@yandex.com>
2025-04-02 15:15:45 +0300

    update task2

--- task2.py
+++ task2.py
@@ -6,7 +6,7 @@ from nltk.corpus import stopwords
 import pymorphy3
 
 DATA_FOLDER = "data"
-TOKENS_FILE = "tokens.txt"
+morph = pymorphy3.MorphAnalyzer()
 
 nltk.download("stopwords")
 russian_stopwords = set(stopwords.words("russian"))
@@ -20,9 +20,12 @@ def tokenize_text(text):
     tokens = set(words) - russian_stopwords
     return tokens
 
-all_tokens = set()
-
 for i in range(1, 101):
+    tokens_file = "tokens/tokens_{}.txt".format(i)
+    lemmas_file = "lemmas/lemmas_{}.txt".format(i)
+    all_tokens = set()
+    lemma_dict = {}
+
     file_path = os.path.join(DATA_FOLDER, f"{i}.txt")
     if not os.path.exists(file_path):
         print(f"Файл {file_path} не найден, пропускаем.")
@@ -34,22 +37,16 @@ for i in range(1, 101):
         tokens = tokenize_text(text)
         all_tokens.update(tokens)
 
-with open(TOKENS_FILE, "w", encoding="utf-8") as out_file:
-    for token in sorted(all_tokens):
-        out_file.write(token + "\n")
+    with open(tokens_file, "w", encoding="utf-8") as out_file:
+        for token in sorted(all_tokens):
+            out_file.write(token + "\n")
 
-# -------------------------
+    for token in all_tokens:
+        lemma = morph.parse(token)[0].normal_form
+        if lemma not in lemma_dict:
+            lemma_dict[lemma] = set()
+        lemma_dict[lemma].add(token)
 
-LEMMAS_FILE = "lemmas.txt"
-morph = pymorphy3.MorphAnalyzer()
-lemma_dict = {}
-
-for token in all_tokens:
-    lemma = morph.parse(token)[0].normal_form
-    if lemma not in lemma_dict:
-        lemma_dict[lemma] = set()
-    lemma_dict[lemma].add(token)
-
-with open(LEMMAS_FILE, "w", encoding="utf-8") as out_file:
-    for lemma, tokens in sorted(lemma_dict.items()):
-        out_file.write(f"{lemma}: {', '.join(sorted(tokens))}\n")
+    with open(lemmas_file, "w", encoding="utf-8") as out_file:
+        for lemma, tokens in sorted(lemma_dict.items()):
+            out_file.write(f"{lemma}: {', '.join(sorted(tokens))}\n")
